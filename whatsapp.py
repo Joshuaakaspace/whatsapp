b462343970138f626cac158e5ec20f0bb7902094
Joshua <75612919+Joshuaakaspace@users.noreply.github.com>
2021-12-10 15:12:01 +0530

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import cv2
+import pywhatkit as kit
+
+
+def main():
+
+    st.title("IOTE")
+    st.text("Built with automation")
+
+    activities = ["whatsapp automater"]
+    choice = st.sidebar.selectbox("Select Acttivity", activities)
+
+    if choice  == 'whatsapp automater':
+        st.subheader("Whatsapp Automater")
+        x = st.text_area('Enter number with Country Code (Example: +91)')
+        y = st.text_area('Enter message')
+        z = st.number_input('Hours', 0, 24)
+        j = st.number_input('Mins', 0, 60)
+        if st.button("Send Message"):  
+
+            kit.sendwhatmsg(x, y, z,j)
+            st.success("Message Sent")
+
+ 
+        #except:
+   
+            # handling exception
+            # and printing error message
+            #st.error("An Unexpected Error!")
+
+
+
+
+    elif choice  =='About':
+        st.subheader("About")
+
+
+
+if __name__ == '__main__':
+    main()	
